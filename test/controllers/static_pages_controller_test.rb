7bda7a942fdfcc970598e03451edf76fe456ff18
kev-kev <kevin.clark93@gmail.com>
2022-05-05 11:24:03 -0400

    Restore the application view layout, add a content page under static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,30 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "| ruby on rails tutorial sample app"
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "home | ruby on rails tutorial sample app"
+    assert_select "title", "home #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "help | ruby on rails tutorial sample app"
+    assert_select "title", "help #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "about | ruby on rails tutorial sample app"
+    assert_select "title", "about #{@base_title}"
+  end
+
+  test "should get content" do
+    get static_pages_content_url
+    assert_response :success
+    assert_select "title", "content #{@base_title}"
   end
 end
