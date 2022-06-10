2f3a3fc33e7f4ccb1c599014b5f9585c6cfd6dc9
kev-kev <kevin.clark93@gmail.com>
2022-06-10 15:48:06 -0400

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,7 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "signing up with invalid info" do
+  test "invalid signup" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: {
@@ -21,4 +21,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
     assert_select 'form ul li', 3
   end
- end
+
+  test "valid signup" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name: "Jean Green",
+          email: "meanjean@bean.cream",
+          password: "abc123",
+          password_confirmation: "abc123",
+        }
+      }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert flash.any?
+  end
