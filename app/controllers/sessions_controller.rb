468e9bb6bd8b5dbe47e8eefea0d6ef7a03d8e27f
kev-kev <kevin.clark93@gmail.com>
2022-06-13 17:29:02 -0400

    add create action for sessions, add test for invalid login flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
-class SessionsController < ApplicationController
+  class SessionsController < ApplicationController
   def new
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log the user in and redirect to user show page
+    else
+      # throw error
+      flash.now[:danger] = 'Invalid email or password'
+      render 'new'
+    end
   end
 
   def destroy
