07c459b53419dc80b8e2014af055253af6389fc1
kev-kev <kevin.clark93@gmail.com>
2022-06-16 15:19:33 -0400

    Add user login tests for valid email/invalid pw and valid email and pw, add test_user fixture, add digest class method to User

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "invalid login" do
+  def setup
+    @user = users(:test_user)
+  end
+
+  test "login with invalid email and password" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
@@ -11,6 +15,35 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "valid login" do
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { 
+      session: { 
+        email: @user.email, 
+        password: ""
+        }
+      }
+    assert_template "sessions/new"
+    assert flash.any?
+    get root_path
+    assert flash.empty?
   end
+
+  test "valid login" do
+    get login_path
+    post login_path, params: {
+      session: {
+        email: @user.email,
+        password: 'password'
+      }
+    }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
 end
