1f32e07c970312fac86b2c4413213f061cd756c5
kev-kev <kevin.clark93@gmail.com>
2022-06-16 15:45:54 -0400

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,5 +14,7 @@
   end
 
   def destroy
+    log_out
+    redirect_to root_path
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -9,6 +9,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert flash.any?
     get root_path
@@ -24,13 +25,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
         password: ""
         }
       }
+    assert_not is_logged_in?
     assert_template "sessions/new"
     assert flash.any?
     get root_path
     assert flash.empty?
   end
 
-  test "valid login" do
+  test "valid login followed by logout" do
     get login_path
     post login_path, params: {
       session: {
@@ -38,12 +40,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
         password: 'password'
       }
     }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 
 end
