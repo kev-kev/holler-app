08e67dc67c51783b92e329fa3f1296497d86cdf5
kev-kev <kevin.clark93@gmail.com>
2022-06-23 17:50:37 -0400

    Set up cookies for user id and remember token for automatic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email or password'
