0bd71a07222c2404e425c433ea34459da611d8fe
kev-kev <kevin.clark93@gmail.com>
2022-06-24 13:25:14 -0400

    add remember me checkbox and functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email or password'
