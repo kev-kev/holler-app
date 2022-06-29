8a2fefb618aea18a7fd64e9499934fa9e07b3767
kev-kev <kevin.clark93@gmail.com>
2022-06-29 15:49:44 -0400

    add before_action method to find user, clean up obsolete controller actions, add update user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
   def new
     @user = User.new
   end
@@ -7,31 +8,32 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the sample app!"
+      flash[:success] = "welcome to the sample app!"
       redirect_to @userco
     else
       render new_user_path, status: 400
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def index
     @users = User.all
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def update
-
+    if @user.update(user_params)
+      flash[:success] = "successfully updated profile"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
