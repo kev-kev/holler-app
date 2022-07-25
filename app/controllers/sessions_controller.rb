38cf2c12cdf1e4d0cbca6503b9f66ba5eeacd28d
kev-kev <kevin.clark93@gmail.com>
2022-07-25 16:58:23 -0400

    add edit action for account activations, only allow sign in for activated users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user&.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+    user =User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = "account not yet activated. please check your email for the activation link."
+        redirect_to root_path 
+      end
     else
       flash.now[:danger] = 'Invalid email or password'
       render 'new'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.raise_delivery_errors = false
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
