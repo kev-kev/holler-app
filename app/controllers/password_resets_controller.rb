ef7775492e511377fcba73b5750952699705c76b
kev-kev <kevin.clark93@gmail.com>
2022-07-27 15:52:44 -0400

    add valid user check and find user before actions on pw resets, create pw reset form, add test for password reset mailer

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :find_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  
   def new
   end
 
@@ -17,5 +20,19 @@ class PasswordResetsController < ApplicationController
   
 
   def edit
+
+  end
+
+  private
+  def find_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    unless (@user && 
+            @user.activated? && 
+            @user.authenticated?(:reset, params[:id]))
+      redirect_to root_path
+    end
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:test_user1)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "account activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
     assert_match user.name.downcase, mail.body.encoded
@@ -13,11 +13,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(user.email), mail.body.encoded
   end
 
-  # test "password_reset" do
-  #   mail = UserMailer.password_reset
-  #   assert_equal "Password reset", mail.subject
-  #   assert_equal ["to@example.org"], mail.to
-  #   assert_equal ["from@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
+  test "password_reset" do
+    user = users(:test_user1)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name.downcase, mail.body.encoded
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
+  end
 end
