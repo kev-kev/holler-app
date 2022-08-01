c4585881072ead492dc629c021581e8a1cf14137
kev-kev <kevin.clark93@gmail.com>
2022-08-01 18:03:30 -0400

    test for user profile with microposts

---
+++ test/integration/users_profile_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UsersProfileTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:test_user1)
+  end
+  test "user profile with microposts" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', text: "#{@user.name}'s page"
+    assert_select 'h1>img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
+end
