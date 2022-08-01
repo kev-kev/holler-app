5f1cd034b256dbe9d125cc61cab200bbb951200c
kev-kev <kevin.clark93@gmail.com>
2022-08-01 17:55:04 -0400

    add seeds for microposts and style micropost list on user show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,8 @@ User.create!(
   )
 end
 
+users = User.first(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content)}
+end
