d1702267d8740262e3b5d9fbe63d98a937b7ada5
kev-kev <kevin.clark93@gmail.com>
2022-08-05 12:07:12 -0400

    add image resizing with imagemagick

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,4 +9,8 @@ class Micropost < ApplicationRecord
     message: "must be a valid image format" },
     size: { less_than: 5.megabytes, 
             message: "should be less than 5MB" }
+
+  def resize_image
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
