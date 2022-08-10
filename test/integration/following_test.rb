29d6a2e3683f79785130c0227f5b9703fc55a70b
kev-kev <kevin.clark93@gmail.com>
2022-08-10 12:29:22 -0400

    add ajax to follow and unfollow forms and tests

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -2,8 +2,9 @@ require "test_helper"
 
 class FollowingTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:test_user1)
+    @user = users(:test_user2)
     log_in_as(@user)
+    @other_user = users(:test_user4)
   end
 
   test "following page" do
@@ -21,6 +22,35 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_select "#followers", @user.followers.count.to_s
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test "should follow a user with html" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: { followed_id: @other_user.id }
+    end
+  end
+
+  test "should follow a user with ajax" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: { followed_id: @other_user.id }
+      # xhr: true
+    end
+  end
+
+  test "should unfollow a user with html" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with ajax" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship), xhr: true
     end
   end
 end
